10fd7cf71dbd727874c0aeca7de0fb4d05cfa317
Mateus Malaquias <mateusmalaquiasdev@outlook.com>
2018-01-30 21:38:02 -0200

    Extraindo responsabilidade de codigo para métodos privados

--- src/com/github/mmalaquiasdev/modulo01/GeradorDeNotaFiscal.java
+++ src/com/github/mmalaquiasdev/modulo01/GeradorDeNotaFiscal.java
@@ -3,8 +3,31 @@ package com.github.mmalaquiasdev.modulo01;
 public class GeradorDeNotaFiscal {
 
 	public NotaFiscal gera(Fatura fatura) {
+		NotaFiscal nf = gerarNotaFiscal(fatura);
+		enviarEmail(nf);
+		salvarNoBanco(nf);
 
-		// calcula valor do imposto
+		return nf;
+	}
+
+	private void salvarNoBanco(NotaFiscal nf) {
+		String sql = "insert into notafiscal (cliente, valor)"+
+					 "values (?," + nf.getValorLiquido() + ")";
+
+		System.out.println("Salvando no banco" + sql);
+	}
+
+	private void enviarEmail(NotaFiscal nf) {
+		String msgDoEmail = "Caro cliente,<br/>";
+		msgDoEmail += "É com prazer que lhe avisamos que sua nota fiscal foi "
+				+ "gerada no valor de " + nf.getValorLiquido() + ".<br/>";
+		msgDoEmail += "Acesse o site da prefeitura e faça o download.<br/><br/>";
+		msgDoEmail += "Obrigado!";
+
+		System.out.println(msgDoEmail);
+	}
+
+	private NotaFiscal gerarNotaFiscal(Fatura fatura) {
 		double valor = fatura.getValorMensal();
 		double imposto = 0;
 		if(valor < 200) {
@@ -16,24 +39,7 @@ public class GeradorDeNotaFiscal {
 		else {
 			imposto = valor * 0.07;
 		}
-		
-		NotaFiscal nf = new NotaFiscal(valor, imposto);
 
-		// envia email
-		String msgDoEmail = "Caro cliente,<br/>";
-		msgDoEmail += "É com prazer que lhe avisamos que sua nota fiscal foi "
-				+ "gerada no valor de " + nf.getValorLiquido() + ".<br/>";
-		msgDoEmail += "Acesse o site da prefeitura e faça o download.<br/><br/>";
-		msgDoEmail += "Obrigado!";
-		
-		System.out.println(msgDoEmail);
-		
-		// salva no banco
-		String sql = "insert into notafiscal (cliente, valor)"+
-					 "values (?," + nf.getValorLiquido() + ")";
-		
-		System.out.println("Salvando no banco" + sql);
-
-		return nf;
+		return new NotaFiscal(valor, imposto);
 	}
 }
