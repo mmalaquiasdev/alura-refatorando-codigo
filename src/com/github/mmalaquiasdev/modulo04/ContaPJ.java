c20f786e18b4c025b30ba3ed1f1cb80cf0a8df59
Mateus Malaquias <mateusmalaquiasdev@outlook.com>
2018-01-30 23:08:22 -0200

    Refatorando, usando herança para evitar código duplicado

--- src/com/github/mmalaquiasdev/modulo04/ContaPJ.java
+++ src/com/github/mmalaquiasdev/modulo04/ContaPJ.java
@@ -1,29 +1,16 @@
 package com.github.mmalaquiasdev.modulo04;
 
-public class ContaPJ {
-
-	private String titular;
-	private double saldo;
+public class ContaPJ extends ContaBancaria{
 
 	public ContaPJ(String titular, double saldoInicial) {
-		this.titular = titular;
-		this.saldo = saldoInicial;
+		super(titular, saldoInicial);
 	}
 	
 	public void saca(double valor) {
-		saldo -= valor + 0.2;
+		super.saca( valor + 0.2);
 	}
 	
 	public void deposita(double valor) {
-		saldo += valor - 0.2;
+		super.deposita(valor - 0.2);
 	}
-	
-	public double getSaldo() {
-		return saldo;
-	}
-	
-	public String getTitular() {
-		return titular;
-	}
-	
 }
