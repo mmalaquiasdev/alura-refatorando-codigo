08f33bcf91a02c9417bf04defa8232f26f9ce5e6
Mateus Malaquias <mateusmalaquiasdev@outlook.com>
2018-01-30 23:21:19 -0200

    Refatorando, removendo parâmetros booleanos

---
+++ src/com/github/mmalaquiasdev/modulo05/Fatura.java
@@ -0,0 +1,35 @@
+package com.github.mmalaquiasdev.modulo05;
+
+public class Fatura {
+
+	private double valorMensal;
+	private String cliente;
+
+	public Fatura() {}
+
+	public Fatura(double valorMensal, String cliente) {
+		this.valorMensal = valorMensal;
+		this.cliente = cliente;
+	}
+	public double getValorMensal() {
+		return valorMensal;
+	}
+	public void setValorMensal(double valorMensal) {
+		this.valorMensal = valorMensal;
+	}
+	public String getCliente() {
+		return cliente;
+	}
+	public void setCliente(String cliente) {
+		this.cliente = cliente;
+	}
+	public double emReal(){
+		return converteValor(1.0);
+	}
+	public double emDolar(){
+		return converteValor(2.7);
+	}
+	private double converteValor(double taxa) {
+		return valorMensal * taxa;
+	}
+}
